91b345614c1ba8f0a051182006eeb28a6f062fed
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-20 02:58:44 +0500

    added Student Configuration class for managing students

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,7 +1,34 @@
 package com.example.demo.student;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository) {
+        return args -> {
+            Student hamid = new Student(
+                        "hamid",
+                        "hamidkalhoro.in@gmail.com",
+                        LocalDate.of(2005, Month.JANUARY, 13)
+            );
+
+            Student muhammad = new Student(
+                    "muhammad",
+                    "muhammad.net@yahoo.com",
+                    LocalDate.of(2014, Month.MARCH, 17)
+            );
+
+            repository.saveAll(
+                    List.of(hamid, muhammad)
+            );
+        };
+    }
 }
