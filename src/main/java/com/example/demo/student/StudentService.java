e32617c71d908cbed5a1a5a255c67cdc6180c688
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-20 02:29:08 +0500

    itialized a new config class for student for adding students with names and there will be a primary key so no need to write a primary key

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,22 +1,20 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "hamid",
-                        "hamidkalhoro.in@gmail.com",
-                        LocalDate.of(2005, Month.JANUARY, 13),
-                        19
-                )
-        );
+        return studentRepository.findAll();
     }
 }
