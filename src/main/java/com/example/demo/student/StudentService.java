209a3a23e7668ef13cc0ed48baf5b97ec1afc870
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-21 04:39:50 +0500

    added delete call to the controller class

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -31,6 +31,13 @@ public class StudentService {
         studentRepository.save(student);
     }
 
-
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists) {
+            throw new IllegalStateException("student with id "
+                    +studentId+" doesn't exists.");
+        }
+        studentRepository.deleteById(studentId);
+    }
 
 }
