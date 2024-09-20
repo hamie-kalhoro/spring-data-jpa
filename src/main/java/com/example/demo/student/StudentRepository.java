15736571cbab0dca25676a9834b9050ade79a165
hamie-kalhoro <aleehamid1024@gmail.com>
2024-09-21 03:29:50 +0500

    added a function for adding student with business logic

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,8 +1,18 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    /*
+        @Query("SELECT s FROM Student s WHERE s.email = ?1") this following
+        expression will do same work as above Query.
+     */
+    Optional<Student> findStudentByEmail(String email);
+
 }
